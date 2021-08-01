cbdba1198d0c4530cbec499a079a69fc574e6808
Siddharth <ssai001@ucr.edu>
2021-08-01 09:47:40 -0700

    First commit

---
+++ Finviz_Web_Scraping.py
@@ -0,0 +1,99 @@
+#%%
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+import requests
+import pandas as pd
+import smtplib
+import sqlalchemy
+
+
+
+#%%
+email_sender_account = "chidachais@gmail.com"
+email_sender_username = "chidachais@gmail.com"  #your email username
+email_sender_password = "Apple07101968!"
+email_smtp_server = "smtp.gmail.com"
+email_smtp_port = 587
+email_recepients = ["siddharthsai@supplychaininc.com"]
+def SendEmail (old,new,date):
+    email_subject = f"Finviz Stock Tracker for {date}"
+    email_body = '<html><head></head><body>'
+    email_body += '<style type="text/css"></style>' 
+    email_body += f'<h2>Finviz Stock Tracker for {date}</h2>' 
+    #old tickers
+    email_body += f'<h1 style="color: rgb(86, 0, 251);">' 
+    email_body += f'<b>Deleted Tickers</b>: ' 
+    email_body += f'{old}</h1>' 
+    #new tickers
+    email_body += f'<h1 style="color: rgb(9, 179, 23);">' 
+    email_body += f'<b>Newly Added Tickers</b>: ' 
+    email_body += f'{new}</h1>' 
+    server = smtplib.SMTP(email_smtp_server,email_smtp_port) 
+    print(f"Logging in to {email_sender_account}")
+    server.starttls() 
+    server.login(email_sender_username, email_sender_password)
+    for recipient in email_recepients:
+        print(f"Sending email to {recipient}")
+        message = MIMEMultipart('alternative') 
+        message['From'] = email_sender_account 
+        message['To'] = recipient 
+        message['Subject'] = email_subject 
+        message.attach(MIMEText(email_body, 'html')) 
+        server.sendmail(email_sender_account,recipient,message.as_string())
+    server.quit()
+DateNow = datetime.date.today()
+
+
+
+#%%
+def TickerDetection(request_url):
+    
+    #Initial Load of the Data
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
+    appended_data = []
+    for i in range(0,100):
+        if i % 20 == 0:
+            screen = requests.get(request_url+"&r="+str(i+1), headers = headers).text
+            tables = pd.read_html(screen)
+            tables = tables[-2]
+            tables.columns = tables.iloc[0]
+            tables = tables[1:]
+            appended_data.append(tables)
+            appended_data_pd = pd.concat(appended_data)
+            appended_data_pd = appended_data_pd.reset_index(drop=True)
+            appended_data_pd = appended_data_pd.drop_duplicates()
+            appended_data_pd_single_col = appended_data_pd['Ticker']
+    engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:sidd1968!@@127.0.0.1:5432/postgres")
+    engine.execute("DELETE FROM finviz_archive")
+    engine.execute("DELETE FROM finviz_stock_screener")
+    engine.execute("DELETE FROM finviz_stock_screener_unique")
+    appended_data_pd_single_col.to_sql('finviz_stock_screener', engine, if_exists='append')
+    engine.execute('INSERT INTO "finviz_stock_screener_unique" select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener where "Datetime" = CURRENT_DATE order by index asc')
+    connection = engine.raw_connection()
+    cursor = connection.cursor()
+    cursor.execute("CALL finviz_all_list();")
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    #Generate old and new data in list format
+    old_result = engine.execute('select "Ticker" from finviz_archive')
+    new_result = engine.execute('select "Ticker" from finviz_stock_screener_unique')
+    engine.dispose()
+    old_tickers = [row[0] for row in old_result]
+    new_tickers = [row[0] for row in new_result]
+    old_tickers_distinct = list(set(old_tickers))
+    new_tickers_distinct = list(set(new_tickers))
+    deleted_tickers = [x for x in old_tickers_distinct if x not in new_tickers_distinct]
+    inserted_tickers = [x for x in new_tickers_distinct if x not in old_tickers_distinct]
+    SendEmail(deleted_tickers,inserted_tickers,DateNow)
+            
+
+
+#%%
+#Pass sample finviz URL's to test functionality
+#req1 = TickerDetection("https://finviz.com/screener.ashx?v=141&f=fa_epsqoq_pos,fa_salesqoq_pos,sh_curvol_o1000,ta_beta_o1,ta_highlow20d_b5h,ta_highlow52w_a70h,ta_sma20_sa50,ta_sma200_sb50,ta_sma50_pa&ft=4&o=-perf13w")
+req2 = TickerDetection("https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o500,sh_price_u40,sh_relvol_o0.75,ta_pattern_tlsupport2&ft=4&o=-volume")
+#print(req1)
+print(req2)
---
+++ finviz_queries.sql
@@ -0,0 +1,115 @@
+select count(*) from finviz_stock_screener; --index, Ticker
+select * from finviz_stock_screener; --index, Ticker
+select count(*) from finviz_archive;        --index, Ticker
+select * from finviz_archive;        --index, Ticker
+
+
+select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener order by index asc; --index, Ticker
+
+select * from finviz_stock_screener where "index" = 35; --index, Ticker
+
+
+
+-- INSERT INTO finviz_stock_screener("Ticker")
+-- VALUES (31, 'SIDDHARTH');
+-- COMMIT;
+
+DELETE FROM finviz_stock_screener;
+DELETE FROM finviz_archive;
+DELETE FROM finviz_stock_screener_unique;
+
+
+
+DROP PROCEDURE finviz_archive;
+
+CREATE OR REPLACE PROCEDURE finviz_archive() 
+AS $$
+BEGIN
+	DELETE FROM finviz_archive;
+	DELETE FROM finviz_stock_screener; --deleting data from finviz_stock_screener deletes finviz_archive data
+END;
+$$
+LANGUAGE PLPGSQL;
+
+
+
+
+CREATE OR REPLACE FUNCTION finviz_audit() RETURNS TRIGGER AS $$
+    BEGIN
+        INSERT INTO finviz_archive 
+        SELECT DISTINCT OLD.* order by index asc;
+        RETURN OLD;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER finviz_archive
+AFTER DELETE ON finviz_stock_screener
+    FOR EACH ROW EXECUTE PROCEDURE finviz_audit();
+
+
+
+create table finviz_stock_screener_unique
+as
+select DISTINCT "index","Ticker","Datetime" 
+from finviz_stock_screener order by index asc; --index, Ticker
+
+
+
+
+select * from finviz_stock_screener_unique;
+select * from finviz_archive;
+select * from finviz_stock_screener;
+select * from finviz_all_list order by "Count" desc;
+-- DELETE FROM finviz_all_list;
+-- ALTER TABLE finviz_all_list ADD CONSTRAINT uniqueticker UNIQUE ("Ticker");
+
+
+
+
+
+select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener where "Datetime" = CURRENT_DATE order by index asc;
+
+
+CREATE OR REPLACE PROCEDURE finviz_all_list() 
+AS $$
+DECLARE
+    REC RECORD;
+BEGIN
+    FOR REC in (SELECT "Ticker" FROM finviz_stock_screener_unique) 
+    LOOP
+        INSERT INTO finviz_all_list ("Count", "Ticker", "Last_Updated_On")
+        VALUES (1, REC."Ticker", CURRENT_DATE)
+        ON CONFLICT ("Ticker")
+        DO
+            UPDATE SET "Count" = finviz_all_list."Count" + 1, "Last_Updated_On" = CURRENT_DATE;
+    END LOOP;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION finviz_delete_archive() RETURNS TRIGGER AS $$
+    BEGIN
+        DELETE FROM finviz_archive;
+        DELETE FROM finviz_stock_screener;
+        DELETE FROM finviz_stock_screener_unique;
+    END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- select customercity,customername,orderamount, avg(orderamount) over (partition by customercity)
+
+--CREATE INDEX idx_persons on PERSONS (firstname, lastname)
+
+--select * from categories where category_name REGEXP 'mon+';
+
+--Important regex symbols
+
+--[[:<:]] and [[:>:]]
+
+--select * from customerstable where firstname REGEXP '^ba';
+
+--ROWS BETWEEN CURRENT ROW AND 3 FOLLOWING
+--ROWS BETWEEN 3 PRECEDING AND CURRENT ROW
+
