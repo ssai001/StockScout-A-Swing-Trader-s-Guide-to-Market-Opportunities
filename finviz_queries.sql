bea7d6c54929142ed8cf48b8500a5c4aa9c65088
Siddharth <ssai001@ucr.edu>
2021-08-05 10:34:20 -0700

    clean up sql queries for better monitoring

--- finviz_queries.sql
+++ finviz_queries.sql
@@ -12,13 +12,25 @@ select count(*) from finviz_archive;
 -----------------------------------------------------------------------------------------------------
 
 
----------------------------Queries to update records from each table---------------------------
+---------------------------Queries to update records from each table (mop-up)---------------------------
 UPDATE finviz_all_list SET "Count" = finviz_all_list."Count" - 1, "Last_Updated_On" = '2021-07-30' 
 WHERE finviz_all_list."Last_Updated_On" = '2021-08-02';
 -----------------------------------------------------------------------------------------------
 
 
----------------------------Queries to alter table---------------------------
+---------------------------Queries to create tables----------------------------------------------
+CREATE TABLE finviz_stock_screener(
+    "Ticker" VARCHAR(20), 
+    "Performance_Month" VARCHAR(20), 
+    "Price" DECIMAL, 
+    "Change" VARCHAR(20), 
+    "Average_Volume" VARCHAR(20), 
+    "Volume" INTEGER
+);
+-----------------------------------------------------------------------------------------------
+
+
+---------------------------Queries to alter tables----------------------------------------------
 ALTER TABLE finviz_all_list ADD CONSTRAINT uniqueticker UNIQUE ("Ticker");
 -----------------------------------------------------------------------------------------------
 
@@ -41,71 +53,3 @@ END;
 $$
 LANGUAGE PLPGSQL;
 -------------------------------------------------------------------------------------------
-
-
----------------------------Queries to develop functions---------------------------
-CREATE OR REPLACE FUNCTION finviz_delete_archive() RETURNS TRIGGER AS $$
-    BEGIN
-        DELETE FROM finviz_archive;
-        DELETE FROM finviz_stock_screener;
-        DELETE FROM finviz_stock_screener_unique;
-    END;
-$$ LANGUAGE plpgsql;
-----------------------------------------------------------------------------------
-
-
-
-select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener order by index asc; --index, Ticker
-
-select * from finviz_stock_screener where "index" = 35; --index, Ticker
-
-
-
--- INSERT INTO finviz_stock_screener("Ticker")
--- VALUES (31, 'SIDDHARTH');
--- COMMIT;
-
-DELETE FROM finviz_stock_screener;
-DELETE FROM finviz_archive;
-DELETE FROM finviz_stock_screener_unique;
-
-
-
-DROP PROCEDURE finviz_archive;
-
-CREATE OR REPLACE PROCEDURE finviz_archive() 
-AS $$
-BEGIN
-	DELETE FROM finviz_archive;
-	DELETE FROM finviz_stock_screener; --deleting data from finviz_stock_screener deletes finviz_archive data
-END;
-$$
-LANGUAGE PLPGSQL;
-
-
-
-
-CREATE OR REPLACE FUNCTION finviz_audit() RETURNS TRIGGER AS $$
-    BEGIN
-        INSERT INTO finviz_archive 
-        SELECT DISTINCT OLD.* order by index asc;
-        RETURN OLD;
-    END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER finviz_archive
-AFTER DELETE ON finviz_stock_screener
-    FOR EACH ROW EXECUTE PROCEDURE finviz_audit();
-
-
-
-create table finviz_stock_screener_unique
-as
-select DISTINCT "index","Ticker","Datetime" 
-from finviz_stock_screener order by index asc; 
-
-
-
-
-
-select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener where "Datetime" = CURRENT_DATE order by index asc;
