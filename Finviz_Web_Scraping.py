5607583312b0edd53c7e7809406e7801c18f8875
Siddharth <ssai001@ucr.edu>
2021-08-06 18:57:20 -0700

    major code change and refactoring

--- Finviz_Web_Scraping.py
+++ Finviz_Web_Scraping.py
@@ -9,11 +9,11 @@ import sqlalchemy
 
 def main():
     req1 = "https://finviz.com/screener.ashx?v=141&f=fa_epsqoq_pos,fa_salesqoq_pos,sh_curvol_o1000,ta_beta_o1,ta_highlow20d_b5h,ta_highlow52w_a70h,ta_sma20_sa50,ta_sma200_sb50,ta_sma50_pa&ft=4&o=-perf13w"
-    req2 = "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o500,sh_price_u40,sh_relvol_o0.75,ta_pattern_tlsupport2&ft=4&o=-volume"
-    TickerDetection(req1,req2)
+    #req2 = "https://finviz.com/screener.ashx?v=111&f=sh_avgvol_o500,sh_price_u40,sh_relvol_o0.75,ta_pattern_tlsupport2&ft=4&o=-volume"
+    TickerDetection(req1)
 
 
-def TickerDetection(*request_url):
+def TickerDetection(request_url):
     
     #Initial Load of the Data
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0'}
@@ -29,30 +29,33 @@ def TickerDetection(*request_url):
             appended_data_pd = pd.concat(appended_data)
             appended_data_pd = appended_data_pd.reset_index(drop=True)
             appended_data_pd = appended_data_pd.drop_duplicates()
-            appended_data_pd_single_col = appended_data_pd['Ticker']
+            appended_data_pd_trimmed = appended_data_pd[['Ticker', 'Perf Month', 'Avg Volume', 'Price', 'Volume']]
     engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:sidd1968!@@127.0.0.1:5432/postgres")
     connection = engine.raw_connection()
     cursor = connection.cursor()
-    cursor.execute("CALL finviz_delete_archive();")
-    appended_data_pd_single_col.to_sql('finviz_stock_screener', engine, if_exists='append')
-    engine.execute('INSERT INTO "finviz_stock_screener_unique" select DISTINCT "index","Ticker","Datetime" from finviz_stock_screener where "Datetime" = CURRENT_DATE order by index asc')
+    appended_data_pd_trimmed.to_sql('finviz_stock_screener', engine, if_exists='replace',
+    dtype={'Ticker': sqlalchemy.VARCHAR(20), 
+                   'Performance_Month':  sqlalchemy.types.VARCHAR(20),
+                   'Price': sqlalchemy.types.DECIMAL,
+                   'Average_Volume': sqlalchemy.types.VARCHAR(20),
+                   'Volume': sqlalchemy.types.BIGINT})
     cursor.execute("CALL finviz_all_list();")
     connection.commit()
     cursor.close()
     connection.close()
 
     #Generate old and new data in list format
-    old_result = engine.execute('select "Ticker" from finviz_archive')
-    new_result = engine.execute('select "Ticker" from finviz_stock_screener_unique')
-    #all_list_result = engine.execute('select * from finviz_all_list')
-    engine.dispose()
-    old_tickers = [row[0] for row in old_result]
-    new_tickers = [row[0] for row in new_result]
-    old_tickers_distinct = list(set(old_tickers))
-    new_tickers_distinct = list(set(new_tickers))
-    deleted_tickers = [x for x in old_tickers_distinct if x not in new_tickers_distinct]
-    inserted_tickers = [x for x in new_tickers_distinct if x not in old_tickers_distinct]
-    SendEmail(deleted_tickers,inserted_tickers,datetime.date.today())
+    # old_result = engine.execute('select "Ticker" from finviz_all_list')
+    # new_result = engine.execute('select "Ticker" from finviz_all_list')
+    # #all_list_result = engine.execute('select * from finviz_all_list')
+    # engine.dispose()
+    # old_tickers = [row[0] for row in old_result]
+    # new_tickers = [row[0] for row in new_result]
+    # old_tickers_distinct = list(set(old_tickers))
+    # new_tickers_distinct = list(set(new_tickers))
+    # deleted_tickers = [x for x in old_tickers_distinct if x not in new_tickers_distinct]
+    # inserted_tickers = [x for x in new_tickers_distinct if x not in old_tickers_distinct]
+    # SendEmail(deleted_tickers,inserted_tickers,datetime.date.today())
 
 
 def SendEmail(old,new,date):
